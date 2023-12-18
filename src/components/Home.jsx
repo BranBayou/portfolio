e763820d7ac4e0b059a1f236799fcf865fc6c0d5
BranBayou <berhan.baye@gmail.com>
2023-12-18 17:28:54 +0300

    Add home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './navigation/Header';
+import Headline from './Headline/Headline';
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Headline />
+    </div>
+  );
+}
+
+export default Home;

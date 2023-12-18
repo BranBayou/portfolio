c9137ea52927f92125dce8f88f0cc61651917812
BranBayou <berhan.baye@gmail.com>
2023-12-18 23:02:37 +0300

    Add more changes

--- src/components/Headline/Intro.jsx
+++ src/components/Headline/Intro.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import ringArrow from '../../Assets/ringArrow.svg';
+import { FiDownload } from "react-icons/fi";
 
 function Intro() {
   return (
-    <div className="">
+    <div className="h-full w-1/2">
       <div className="">
-        <img className="" src={ringArrow} alt="arrow" />
+        <img className="absolute top-52" src={ringArrow} alt="arrow" />
       </div>
-      <div className="w-1/2">
+      <div className="h-full w-9/12 mx-auto flex flex-col gap-20 items-start justify-center">
         <div className="">
-          <h1 className="">
-            Hi I&apos;m Bran,a Creative
+          <h1 className="text-4xl font-bold">
+            Hi I&apos;m Bran,A Full-Stack
             {' '}
-            <span className="">
-              UI Designer.
+            <span className="text-aquab text-4xl font-bold">
+              DEVELOPER.
             </span>
           </h1>
-        </div>
-        <p className="">
+          
+          <p className="mt-9">
           Passionate about creating visually
           appealing and user-friendly websites,
           focusing on seamless user interfaces and
           engaging experiences.
-        </p>
-        <div className="">
-          <button className="" href="https://www.linkedin.com/in/bran-baye/">Hire me</button>
-          <button className="" href="https://docs.google.com/document/d/1dM-S4qVg-4mAeqKDHwAXpnUdvD9nyQF6BXXPGFv8ybA/edit?usp=sharing">Download my CV</button>
+          </p>
+        </div>
+        <div className="flex gap-6">
+          <button className="w-36 bg-darkt p-2 rounded-3xl border hover:bg-aquab" href="https://www.linkedin.com/in/bran-baye/">Hire me</button>
+          <button className="flex items-center justify-center gap-3 w-56 bg-darkt p-2 rounded-3xl border hover:bg-aquab" href="https://docs.google.com/document/d/1dM-S4qVg-4mAeqKDHwAXpnUdvD9nyQF6BXXPGFv8ybA/edit?usp=sharing">Download my CV <FiDownload /></button>
         </div>
       </div>
     </div>
--- src/components/Headline/ProfilePhoto.jsx
+++ src/components/Headline/ProfilePhoto.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import bran from '../../Assets/bran.jpg';
+import nextP from '../../Assets/nextP.svg';
 
 function ProfilePhoto() {
   return (
-    <div className="">
-      <img className="w-72" src={bran} alt="avatar" />
+    <div className="w-1/2 flex items-center justify-center">
+      <div className="">
+        <img className="w-72" src={bran} alt="avatar" />
+      </div>
+      <div className="">
+        <img className="w-12 absolute bottom-24 left-96 border" src={nextP} alt="next page" />
+      </div>
     </div>
   );
 }

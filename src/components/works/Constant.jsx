8d7d44a405c3e69182753a3d38c42cc672b56348
BranBayou <berhan.baye@gmail.com>
2023-12-19 22:20:40 +0300

    Add constant data

---
+++ src/components/works/Constant.jsx
@@ -0,0 +1,39 @@
+  const Constant = [
+    {
+      title: "Some title one",
+      shortDes: "Some short desc",
+      desc: "Some random description goes here",
+      img: "some url",
+      stack: ["Html5", "Css3", "JavaScript", "Ruby"],
+    },
+    {
+      title: "Some title Two",
+      shortDes: "Some short desc",
+      desc: "Some random description goes here",
+      img: "some url",
+      stack: ["Html5", "Css3", "React"],
+    },
+    {
+      title: "Some title Three",
+      shortDes: "Some short desc",
+      desc: "Some random description goes here",
+      img: "some url",
+      stack: ["Html5", "Css3", "JavaScript"],
+    },
+    {
+      title: "Some title Four",
+      shortDes: "Some short desc",
+      desc: "Some random description goes here",
+      img: "some url",
+      stack: ["Html5", "Css3", "JavaScript"],
+    },
+    {
+      title: "Some title Five",
+      shortDes: "Some short desc",
+      desc: "Some random description goes here",
+      img: "some url",
+      stack: ["Html5", "Css3", "JavaScript"],
+    },
+  ];
+
+export default Constant;

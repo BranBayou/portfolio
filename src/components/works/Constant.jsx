9e8c112219db342bfa96adf9d0ef5b8adedc5610
BranBayou <berhan.baye@gmail.com>
2023-12-20 08:12:09 +0300

    Update constant component

--- src/components/works/Constant.jsx
+++ src/components/works/Constant.jsx
@@ -1,38 +1,64 @@
+import mealC1 from '../../Assets/constants/mealC1.jpeg';
+import mealC2 from '../../Assets/constants/mealC2.jpeg';
+import mealC3 from '../../Assets/constants/mealC3.jpeg';
+import mealC4 from '../../Assets/constants/mealC4.jpeg';
+import bookS1 from '../../Assets/constants/bookStoreD.png';
+import bookS2 from '../../Assets/constants/bookStoreM.png';
+import bud1 from '../../Assets/constants/bud1.png';
+import bud2 from '../../Assets/constants/bud2.png';
+import bud3 from '../../Assets/constants/bud3.png';
+import randC1 from '../../Assets/constants/randC1.png';
+import randC2 from '../../Assets/constants/randC2.png';
+import randC3 from '../../Assets/constants/randC3.png';
+import randC4 from '../../Assets/constants/randC4.png';
+import blog1 from '../../Assets/constants/blogD.png';
+import blog2 from '../../Assets/constants/blogM.png';
+  
   const Constant = [
     {
-      title: "Some title one",
-      shortDes: "Some short desc",
-      desc: "Some random description goes here",
-      img: "some url",
-      stack: ["Html5", "Css3", "JavaScript", "Ruby"],
+      title: "Meal-connect",
+      shortDes: "Full-satck app",
+      desc: "MealConnect is a vibrant web application designed for those who appreciate the art of cooking and the pleasure of savoring exquisite meals. Dive into a world of flavors, discover new recipes, and make customized reservations based on your taste buds.",
+      img: [mealC1, mealC2, mealC3, mealC4],
+      stack: ["React", "Tailwind", "Ruby on Rails"],
+      liveDemo: 'https://deployment--eloquent-malabi-441830.netlify.app/login',
+      gitHub: 'https://github.com/BranBayou/meal_connect-frontend',
     },
     {
-      title: "Some title Two",
-      shortDes: "Some short desc",
-      desc: "Some random description goes here",
-      img: "some url",
-      stack: ["Html5", "Css3", "React"],
+      title: "Bookstore app",
+      shortDes: "API app",
+      desc: "Bookstore app is React-based Web App leverages an API to deliver a dynamic and responsive online bookstore experience. Users can add, delete, see chapters, and easily store a wide variety of books through an intuitive web interface.",
+      img: [bookS1, bookS2],
+      stack: ["React", "Css3", "public Api"],
+      liveDemo: 'https://brans-bookstore-app.onrender.com/',
+      gitHub: 'https://github.com/BranBayou/react-bookstore',
     },
     {
-      title: "Some title Three",
-      shortDes: "Some short desc",
-      desc: "Some random description goes here",
-      img: "some url",
+      title: "My budget",
+      shortDes: "Full-stack app",
+      desc: "My-budget app is a web app that has Ruby on Rails back-end, it lets you manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what.",
+      img: [bud1, bud2, bud3],
       stack: ["Html5", "Css3", "JavaScript"],
+      liveDemo: 'https://my-budget-hq38.onrender.com/',
+      gitHub: 'https://github.com/BranBayou/my-budget',
     },
     {
-      title: "Some title Four",
-      shortDes: "Some short desc",
-      desc: "Some random description goes here",
-      img: "some url",
-      stack: ["Html5", "Css3", "JavaScript"],
+      title: "Random cities",
+      shortDes: "React app",
+      desc: "Random Cities React App is an interactive and dynamic application built using React.js that allows users to explore various cities information worldwide utilizing API.",
+      img: [randC1, randC2, randC3, randC4],
+      stack: ["Css3", "React"],
+      liveDemo: 'https://world-cities-random.onrender.com/',
+      gitHub: 'https://github.com/BranBayou/random-cities',
     },
     {
-      title: "Some title Five",
-      shortDes: "Some short desc",
+      title: "Personal blog",
+      shortDes: "Front end app",
       desc: "Some random description goes here",
-      img: "some url",
+      img: [blog1, blog2],
       stack: ["Html5", "Css3", "JavaScript"],
+      liveDemo: 'https://branbayou.github.io/capstonePro1-personal-blog/',
+      gitHub: 'https://github.com/BranBayou/capstonePro1-personal-blog',
     },
   ];
 

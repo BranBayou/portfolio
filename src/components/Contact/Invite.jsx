ed8045675c5a6978e84a08ceafe3da6adee0caee
BranBayou <berhan.baye@gmail.com>
2023-12-22 07:53:53 +0300

    Add contact mobile version

--- src/components/Contact/Invite.jsx
+++ src/components/Contact/Invite.jsx
@@ -4,12 +4,12 @@ import grow from '../../Assets/grow.svg'
 
 const Invite = () => {
   return (
-    <div className="w-1/2">
-      <div className="flex flex-col">
+    <div className="w-full md:w-1/2 flex flex-col items-center md:items-start">
+      <div className="flex flex-col items-center md:items-start">
         <h1 className="text-5xl font-bold">Got a project in</h1>
         <span className="text-aquab text-5xl font-bold">mind?</span>
       </div>
-      <div className="flex">
+      <div className="flex items-center">
         <img src={arrow2} alt="arrow" />
         <img src={grow} alt="arrow" />
       </div>

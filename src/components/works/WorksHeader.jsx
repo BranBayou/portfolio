7832378e748d30647bb7d62b8d901135e38e8d3a
BranBayou <berhan.baye@gmail.com>
2023-12-23 18:46:49 +0300

    Add responsive works section

--- src/components/works/WorksHeader.jsx
+++ src/components/works/WorksHeader.jsx
@@ -5,7 +5,7 @@ const WorksHeader = () => {
   return (
     <div className="h-full">
       <div className="w-full flex items-center justify-center mb-12 py-12 border-b">
-       <h1 className="text-4xl">
+       <h1 className="text-4xl font-bold">
          My recent <span className="text-aquab">works</span>
        </h1>
       </div>

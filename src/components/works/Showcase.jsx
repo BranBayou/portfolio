1bf64e0079f7b5e5815b48c5d36496e19a9caad0
BranBayou <berhan.baye@gmail.com>
2023-12-24 08:58:34 +0300

    Add popup screen

--- src/components/works/Showcase.jsx
+++ src/components/works/Showcase.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useState } from 'react';
 import Constant from './Constant';
+import Popup from './Popup';
 
 const Showcase = () => {
+
+  const [showPopup, setShowPopup] = useState(false);
+  const [selectedProjectIndex, setSelectedProjectIndex] = useState(null);
+
+  const openPopup = (index) => {
+    setSelectedProjectIndex(index);
+    setShowPopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
+
   return (
     <div className="w-full grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 ib:grid-cols-3  gap-4">
       {Constant.map((data, index) => (
-        <div key={index} className={`bg-darkt p-3 rounded-3xl`}>
+        <div key={index} className={`bg-darkt border p-3 rounded-3xl`}>
           <div className="mb-2">
             <img src={data.img[0]} className="w-full rounded-3xl" alt="img" />
           </div>
@@ -15,19 +30,27 @@ const Showcase = () => {
               <p>{data.shortDes}</p>
             </div>
             <div >
-              <p>{data.desc}</p>
+              
               <div className="flex gap-3 my-2">
                 <button className="bg-darkt border px-3 rounded-3xl">{data.stack[0]}</button>
                 <button className="bg-darkt border px-3 rounded-3xl">{data.stack[1]}</button>
                 <button className="bg-darkt border px-3 rounded-3xl">{data.stack[2]}</button>
               </div>
             </div>
-            <div className="flex gap-6">
+            <div className="flex gap-6" onClick={() => openPopup(index)}>
               <button type="button" className="w-full md:w-36 bg-aquab px-3 py-1 rounded-3xl hover:bg-darkt hover:border active:bg-darkt active:border">See Project</button>
             </div>
           </div>
         </div>
       ))}
+       {showPopup && (
+        <div className="popup-container">
+          <div className="popup-content">
+            {/* Pass necessary props to the Popup component */}
+            <Popup data={Constant[selectedProjectIndex]} closePopup={closePopup} />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
